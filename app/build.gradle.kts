762a728a9f12786feaff987dfe704471646fcfc8
Akhil Tyagi <akhiltyagi@tutamail.com>
2026-08-15 15:58:46 +0530

    Bump version to 1.2 to trigger in-app update with new Sleep and Gestures features

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.example.un_signed"
         minSdk = 29
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
