b01fdf9423555578be322da640e9f6663bd975b6
Akhil Tyagi <akhiltyagi@tutamail.com>
2026-08-22 18:29:45 +0530

    Bump version to 1.5, update version.json, and sync latest APK to root for GitHub

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.example.un_signed"
         minSdk = 29
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
